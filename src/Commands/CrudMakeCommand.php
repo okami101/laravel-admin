2f6ec9fe7490f09fdf592e0137093c697f5eef2b
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-04-25 19:30:10 +0200

    add id

--- src/Commands/CrudMakeCommand.php
+++ src/Commands/CrudMakeCommand.php
@@ -189,7 +189,7 @@ class CrudMakeCommand extends GeneratorCommand
             '{{ modelVariable }}',
             '{{ user }}',
         ], [
-            $this->getArrayString($this->getFields()->keys()),
+            $this->getArrayString(['id', ...$this->getFields()->keys()]),
             $this->getArrayWithKeysString($this->getCasts()),
             $this->getArrayString($this->getTranslatableFields()),
             $this->getArrayString($this->getSearchableFields()),
